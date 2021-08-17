19e17fc7d685f96f5218ccd67242644e053e2c5f
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2021-08-17 17:34:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,22 +11,22 @@ class App extends Component {
 
     render() {
     	return(
-    	    <div id="main">
+    	   <div id="main">
 	      <button 
 		id ='click' 
 		onClick={() => {
 		 this.setState({pHidden:false});
-	}}
-	>
+	        }}
+	      >
 		Show Para
-               </button>
+              </button>
                {this.state.pHidden ? null :(
 	        <p id ="para">
 	          Hello,I've learnt to use the full-stack evaluation tool.This makes me so happy
 
-	        </p>
+	        </p> 
 	     )}
-            </div>
+           </div>
     	);
     }
 }
